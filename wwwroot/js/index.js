87d1ea119126f053f3875a9d71702f79468c1244
Michael Agbenyegah <Agbenyegahm@accessbankplc.com>
2025-10-20 13:05:11 +0000

    Update: Vue.js

---
+++ wwwroot/js/index.js
@@ -0,0 +1,28 @@
+﻿const app = Vue.createApp({
+
+    data() {
+        return {
+            currencies: []
+        };
+    },
+    methods: {
+        fetchCurrencies() {
+            fetch('/api/visa/currencies')
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Currencies API response:', data);
+                    this.currencies = data.result || data.Result || [];
+                })
+                .catch(error => {
+                    console.error('Error fetching currencies:', error);
+                    this.currencies = [];
+                });
+        }
+    },
+    mounted() {
+        this.fetchCurrencies();
+    }
+
+});
+
+app.mount('#app');
